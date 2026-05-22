59320c8a87e6bfc790933b5d3fb897be0e85d8cd
Simranpreet Singh <saini.devX@gmail.com>
2026-05-22 15:20:18 +0530

    Modify CORS settings to specify allowed origin
    Updated CORS configuration to allow requests from a specific origin.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,43 @@
+// import express from "express";
+// import connectDB from "./config/db.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import paymentRoute from "./routes/payment.js";
+
+// const app = express();
+
+// dotenv.config();
+
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/payment", paymentRoute);
+// app.use("/api/products", productRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port http://localhost:${PORT}`);
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
 import express from "express";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -9,9 +49,12 @@ const app = express();
 
 dotenv.config();
 
-
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: "https://techverse-shop.vercel.app",
+  credentials: true
+}));
 
 app.use("/api/payment", paymentRoute);
 app.use("/api/products", productRoutes);
